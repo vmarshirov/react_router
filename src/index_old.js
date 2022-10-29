05b0e75563b74d38445d0bf880bc2e769fb9e0dd
vmarshirov <vmarshirov@outlook.com>
2022-10-29 13:00:36 +0300

    000

---
+++ src/index_old.js
@@ -0,0 +1,21 @@
+// import { createRoot } from "react-dom/client";
+// import App from "./App";
+//
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+//
+// root.render(
+//      <App />
+//  );
+
+
+
+import { createRoot } from "react-dom/client";
+import App from "./Main";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+     <App />
+ );
