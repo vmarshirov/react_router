521f178df452f9eabdd65768367751bb7d1788de
vmarshirov <vmarshirov@outlook.com>
2022-11-21 22:11:38 +0300

    2022-11-21_04

--- src/routes/try.jsx
+++ src/routes/try.jsx
@@ -54,16 +54,11 @@ const Try = () => {
                 <input name="formulation" type="hidden"/>
                 <fieldset id={"fields"}>
                     <label id={"label"}>Задача: <input style={{width: '80%'}} name="task" type="text" value={task}/></label>
-                    <label id={"label"}>a: <input name="a" size="4" type="number" onChange={(e) => setA(e.target.value)}
-                                                  value={a}/></label>
-                    <label id={"label"}>b: <input name="b" size="4" type="number" onChange={(e) => setB(e.target.value)}
-                                                  value={b}/></label>
-                    <label id={"label"}>c: <input name="c" size="4" type="number" onChange={(e) => setC(e.target.value)}
-                                                  value={c}/></label>
-
+                    <label id={"label"}>a: <input name="a" size="4" type="number" onChange={(e) => setA(e.target.value)} value={a}/></label>
+                    <label id={"label"}>b: <input name="b" size="4" type="number" onChange={(e) => setB(e.target.value)} value={b}/></label>
+                    <label id={"label"}>c: <input name="c" size="4" type="number" onChange={(e) => setC(e.target.value)} value={c}/></label>
                     <label id={"label"}>Результаты: <input style={{width: '80%'}} name="result" type="text" value={result}/></label>
                 </fieldset>
-
                 <label><input id={"verify"} type={"button"} value={"Проверить"}/></label>
                 <label><input id={"send"} type={"button"} value={"Отправить"} onClick={send}/></label>
 
